25ec6a138271be2b25404698e026d1dd16bca587
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-04 00:13:03 -0300

    Tratando com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const server = fastify()
 
@@ -14,7 +14,7 @@ server.get('/hello', async () => {
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')

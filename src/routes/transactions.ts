82150e401cb86d4d5273ae6f5bcfa2412d83f172
Clebão 🥲 <cleber.abreu@globalsys.com.br>
2023-07-06 16:14:34 -0300

    Listagem das transações e transação por id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,34 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select('*')
+
+        return {
+            transactions,
+        }
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions').select('*').where({ id }).first()
+
+        if (!transaction) {
+            return {
+                error: 'Transaction not found',
+            }
+        }
+
+        return {
+            transaction,
+        }
+    })
+
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
@@ -11,8 +39,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
             type: z.enum(['credit', 'debit']),
         })
 
-        const { title, amount, type } = createTransactionBodySchema
-            .parse(request.body)
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
         await knex('transactions').insert({
             id: randomUUID(),
